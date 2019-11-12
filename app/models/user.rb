b7da7b27c4b96c065b0a6efce58283c04091197a
Ben Black <benjyblack@gmail.com>
2019-11-11 20:09:21 -0800

    Add Follower relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   validates_uniqueness_of :username
+
+  has_many :follower_relationships, foreign_key: :followee_id, class_name: 'Follower'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :followee_relationships, foreign_key: :follower_id, class_name: 'Follower'
+  has_many :followees, through: :followee_relationships, source: :followee
 end
