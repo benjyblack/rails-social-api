3e03d7a31e882afea521dae50e389827d88f1f68
Ben Black <benjyblack@gmail.com>
2019-11-11 20:00:02 -0800

    Add follower tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_034115) do
+ActiveRecord::Schema.define(version: 2019_11_12_035358) do
+
+  create_table "followers", force: :cascade do |t|
+    t.integer "followee_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followee_id", "follower_id"], name: "index_followers_on_followee_id_and_follower_id", unique: true
+    t.index ["followee_id"], name: "index_followers_on_followee_id"
+    t.index ["follower_id"], name: "index_followers_on_follower_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
