e8df5dd9e45b1edef0379a9971c54546c35843b1
Ben Black <benjyblack@gmail.com>
2019-11-11 19:49:25 -0800

    Bootstrap factory bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to be_a(described_class) }
+
+  describe 'validations' do
+    describe 'uniqueness' do
+      subject { FactoryBot.build(:user) }
+
+      context 'when no other user exists with the same username' do
+        it { is_expected.to be_valid }
+      end
+    end
+  end
 end
